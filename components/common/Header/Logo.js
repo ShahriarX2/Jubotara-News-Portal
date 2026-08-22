4620e933048e4a5dffc90aaab26d4a45ca4da199
Shahriar Hossen <cocguru9@gmail.com>
2026-08-22 15:14:06 +0600

    Remove logoUrl from logo.js

--- components/common/Header/Logo.js
+++ components/common/Header/Logo.js
@@ -2,13 +2,13 @@ import Link from 'next/link';
 import jtLogo from '@/public/images/logo.png';
 import rpcLogo from '@/public/images/rpcLogo.jpg';
 
-const Logo = ({ logoUrl, className }) => {
+const Logo = ({ className }) => {
   const selectedLogo = process.env.FRONTEND_URL === 'https://jubotaranews.com' ? jtLogo : rpcLogo;
   return (
     <Link href="/" className="relative z-60 flex items-center shrink-0">
       <div className={`relative transition-transform duration-200 hover:scale-105 ${className}`}>
         <img
-          src={logoUrl || selectedLogo.src || selectedLogo}
+          src={selectedLogo.src || selectedLogo}
           alt="Jubotara News Logo"
           sizes="(max-width: 768px) 150px, 200px"
           className="object-contain absolute inset-0 w-full h-full"
